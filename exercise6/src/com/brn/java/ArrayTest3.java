ca43c855e14e201adf675eefdcebfdbd3fdd7dec
BrnBrian <brianelicc@gmail.com>
2022-05-04 22:34:32 -0400

    ArrayTest3

---
+++ exercise6/src/com/brn/java/ArrayTest3.java
@@ -0,0 +1,26 @@
+package com.brn.java;
+
+/**
+ * 
+ * @description
+ * @author BRN_Brian
+ * @date 2022年5月4日下午9:53:42
+ * @version 
+ *
+ */
+
+public class ArrayTest3 {
+	public static void main(String[] args) {
+		
+		int[] arr1 = new int[]{1,2,3};//1
+		
+		int[][] arr2 = new int[][]{{1,2,3},{4,7},{9,7}};//static 2
+		String[][] arr3 = new String[3][2];//dynamic 2
+		String[][] arr4 = new String[3][];//dynamic 2
+		
+		System.out.println(arr2[0][2]);
+		
+
+	}
+
+}
